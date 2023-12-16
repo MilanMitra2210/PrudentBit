0c514fd1272736299313a0b37c06da068a8c5db8
Milan Mitra <milanmitranda2020@gmail.com>
2023-12-16 16:31:04 +0530

    api fetching done

--- front-assesment/src/components/Mainpage.js
+++ front-assesment/src/components/Mainpage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./Mainpage.css";
 import  musicIcon from "../images/music-icon.png";
 import  home from "../images/home.png";
@@ -8,11 +8,45 @@ import  artist from "../images/artists.png";
 import  album from "../images/albun.png";
 import  favroites from "../images/favorites.png";
 import  podcast from "../images/Podcasts.png";
-// import  podcast from "../images/Podcasts.png";
+import  leftarrow from "../images/leftarrow.png";
+import  rightarrow from "../images/rightarrow.png";
+import  setting from "../images/setting.png";
+import  bell from "../images/bell.png";
 import { HiMenuAlt3 } from "react-icons/hi";
-
+import axios from 'axios';
 
 export default function Mainpage() {
+  
+  const [playlistData, setPlaylistData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const options = {
+        method: 'GET',
+        url: 'https://spotify23.p.rapidapi.com/playlist/',
+        params: {
+          id: '37i9dQZF1DX4Wsb4d7NKfP'
+        },
+        headers: {
+          'X-RapidAPI-Key': '9da8a1ad2dmsh293fca8c5c9cf00p12260fjsn7e35b6ccb817',
+          'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = await axios.request(options);
+        setPlaylistData(response.data);
+        console.log(response.data);
+        console.log(playlistData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  
     return (
         <div className="container-fluid">
           <div className='left'>
@@ -34,6 +68,37 @@ export default function Mainpage() {
           <div className="right">
             <div className="navBar">
                 <div>Home</div>
+                <div><img src={leftarrow} alt="..." className="img-thumbnail" width="28" height="28" /></div>
+                <div><img src={rightarrow} alt="..." className="img-thumbnail" width="28" height="28" /></div>
+                <div className="searchBar">
+                <input type="password" className="form-control" placeholder="Search" />
+                </div>
+                <div><img src={setting} alt="..." className="img-thumbnail" width="34" height="34" /></div>
+                <div><img src={bell} alt="..." className="img-thumbnail" width="34" height="34" /></div>
+                <div>
+                  <div>Hello</div>
+                  <div>Sharda University</div>
+                </div>
+            </div>
+
+            <div className="playlist">
+            
+              {playlistData? (
+                {playlistData.data.map((playlistItem) => (
+                  <div class="card">
+                <img src={playlistItem.images[0].url} class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text" >{playlistItem.images[0].name}</p>
+                </div>
+              </div>
+              )}
+
+              {/* <div class="card">
+                <img src="..." class="card-img-top" alt="..."/>
+                <div class="card-body">
+                  <p class="card-text"></p>
+                </div>
+              </div> */}
 
             </div>
           </div>
